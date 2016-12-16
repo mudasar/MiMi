bd0bfda436e98af00e221865b560499021e3470d
mudasar <mudassar.rauf@gmail.com>
2016-12-16 16:24:43 +0000

    Initial Commit

---
+++ MiMi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiMi
+//
+//  Created by mudasar on 16/12/2016.
+//  Copyright © 2016 appinvent.uk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
